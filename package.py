7bcdc26504e41f9d362d846778dbdb4109cbb5a4
janishsethu <52109147+janishsethu@users.noreply.github.com>
2021-02-15 14:46:55 +0530

    package
    demo program to study git hub

---
+++ package.py
@@ -0,0 +1,17 @@
+from graphics import circle,rectangle
+import sys
+print("circle")
+circle.areac()
+circle.peric()
+print("rectangle")
+rectangle.areap()
+rectangle.perip()
+sys.path.append(r"I:\documents\mca\python lab\programs\package\graphics\3DGraphics")
+from cuboid import *
+print("cuboid")
+areacu()
+volucu()
+from sphere import *
+print("sphere")
+areas()
+volus()
